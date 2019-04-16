6ce8a097d74f4a2057e589d8abf4715158e89f8b
wzx <819547198@qq.com>
2019-04-16 12:02:36 +0800

    数据库

---
+++ demo1/task/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+
+class BookInfo(models.Model):
+    btitle = models.CharField(max_length=20)
+    bpub_data = models.DateTimeField()
+
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=20)
+    hgender = models.BooleanField()
+    hcontent = models.CharField(max_length=100)
+    hbook = models.ForeignKey("BookInfo",on_delete=models.CASCADE)
+
